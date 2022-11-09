0782ffdfefd2e0e157f2b235442c01211ae95c17
refresh88 <ouok88@gmail.com>
2022-11-09 21:22:51 +0900

    carrotGame: refactoring_1

--- src/main.js
+++ src/main.js
@@ -1,21 +1,15 @@
 'use strict';
+import PopUp from './popup.js';
+import Field from './field.js';
 
-const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
 const GAME_DURATION_SEC = 5;
 
-const field = document.querySelector('.game__field');
-const fieldRect = field.getBoundingClientRect();
 const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
-const popUp = document.querySelector('.pop-up');
-const popUpText = document.querySelector('.pop-up__message');
-const popUpRefresh = document.querySelector('.pop-up__refresh');
-
-const carrotSound = new Audio('./sound/carrot_pull.mp3');
 const alertSound = new Audio('./sound/alert.mp3');
 const bgSound = new Audio('./sound/bg.mp3');
 const bugSound = new Audio('./sound/bug_pull.mp3');
@@ -25,7 +19,28 @@ let started = false;
 let score = 0;
 let timer = undefined;
 
-field.addEventListener('click', onFieldClick);
+const gameFinishBanner = new PopUp();
+gameFinishBanner.setClickListener(() => {
+  startGame();
+});
+
+const gameField = new Field(CARROT_COUNT, BUG_COUNT);
+gameField.setClickListener(onItemClick);
+
+function onItemClick(item) {
+  if (!started) {
+    return;
+  }
+  if (item === 'carrot') {
+    score++;
+    updateScoreBoard();
+    if (score === CARROT_COUNT) {
+      finishGame(true);
+    }
+  } else if (item === 'bug') {
+    finishGame(false);
+  }
+}
 
 gameBtn.addEventListener('click', () => {
   if (started) {
@@ -33,12 +48,6 @@ gameBtn.addEventListener('click', () => {
   } else {
     startGame();
   }
-});
-
-popUpRefresh.addEventListener('click', () => {
-  startGame();
-  hidePopUp();
-  popUp.classList.add('pop-up--hide');
 });
 
 function startGame() {
@@ -54,7 +63,7 @@ function stopGame() {
   started = false;
   stopGameTimer();
   hideGameButton();
-  showPopUpWithText('REPLAY❓');
+  gameFinishBanner.showWithText('REPLAY❓');
   playSound(alertSound);
   stopSound(bgSound);
 }
@@ -69,7 +78,7 @@ function finishGame(win) {
   }
   stopGameTimer();
   stopSound(bgSound);
-  showPopUpWithText(win ? 'YOU WON !!' : 'YOU LOST :(');
+  gameFinishBanner.showWithText(win ? 'YOU WON !!' : 'YOU LOST :(');
 }
 
 function showStopButton() {
@@ -111,40 +120,10 @@ function updateTimerText(time) {
   gameTimer.innerText = `${minutes}:${seconds}`;
 }
 
-function showPopUpWithText(text) {
-  popUpText.innerText = text;
-  popUp.classList.remove('pop-up--hide');
-}
-
-function hidePopUp() {
-  popUp.classList.add('pop-up--hide');
-}
-
 function initGame() {
   score = 0;
-  field.innerHTML = '';
   gameScore.innerText = CARROT_COUNT;
-  // 벌레, 당근 생성한후 field에 추가
-  addItem('carrot', CARROT_COUNT, 'img/carrot.png');
-  addItem('bug', BUG_COUNT, 'img/bug.png');
-}
-
-function onFieldClick(event) {
-  if (!started) {
-    return;
-  }
-  const target = event.target;
-  if (target.matches('.carrot')) {
-    target.remove();
-    score++;
-    playSound(carrotSound);
-    updateScoreBoard();
-    if (score === CARROT_COUNT) {
-      finishGame(true);
-    }
-  } else if (target.matches('.bug')) {
-    finishGame(false);
-  }
+  gameField.init();
 }
 
 function playSound(sound) {
@@ -158,23 +137,6 @@ function stopSound(sound) {
 
 function updateScoreBoard() {
   gameScore.innerText = CARROT_COUNT - score;
-}
-function addItem(className, count, imgPath) {
-  const x1 = 0;
-  const y1 = 0;
-  const x2 = fieldRect.width - CARROT_SIZE;
-  const y2 = fieldRect.height - CARROT_SIZE;
-  for (let i = 0; i < count; i++) {
-    const item = document.createElement('img');
-    item.setAttribute('class', className);
-    item.setAttribute('src', imgPath);
-    item.style.position = 'absolute';
-    const x = randomNumber(x1, x2);
-    const y = randomNumber(y1, y2);
-    item.style.left = `${x}px`;
-    item.style.top = `${y}px`;
-    field.appendChild(item);
-  }
 }
 
 function randomNumber(min, max) {
